a7d9bf532784c6417821ff2a2148107c16ef33cc
ranaintizar <intizaralirana2@gmail.com>
2023-05-30 06:24:03 +0500

    Font Fixing for Build

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,12 @@ export default function Home() {
         <meta name="description" content="Codezar | Web Agency" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=DM+Sans:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <main>
         <Header />
